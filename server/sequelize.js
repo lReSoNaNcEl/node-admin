7d9ef192416879d0585a112578dffeed561241bc
Resonance <iresonancei@mail.ru>
2020-09-19 21:48:17 +0300

    Save

--- server/sequelize.js
+++ server/sequelize.js
@@ -2,7 +2,8 @@ const {Sequelize} = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DB_DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST || 'localhost',
-    dialect: process.env.DB_NAME || 'postgres'
+    dialect: process.env.DB_NAME || 'postgres',
+    port: process.env.DB_PORT || '5432'
 })
 
 new Promise(async resolve => await sequelize.authenticate())
